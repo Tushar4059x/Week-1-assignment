ddab78043d0e190a4d86176652cf034e3c739984
Tushar4059x <gulyanitushar@gmail.com>
2023-06-12 17:19:33 +0530

    just wrote the code for anagram in js which will run on node js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -1,14 +1,36 @@
-/*
-  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
-  What's Anagram?
-  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
-
-  Once you've implemented the logic, test your code by running
-  - `npm run test-anagram`
-*/
+const readline = require('readline');
 
 function isAnagram(str1, str2) {
+  str1 = str1.replace(/\s/g, '').toLowerCase();
+  str2 = str2.replace(/\s/g, '').toLowerCase();
 
+  if (str1.length !== str2.length) {
+    return false;
+  } else {
+    const str1Arr = str1.split('');
+    const str2Arr = str2.split('');
+
+    str1Arr.sort();
+    str2Arr.sort();
+
+    const sortedStr1 = str1Arr.join('');
+    const sortedStr2 = str2Arr.join('');
+
+    return sortedStr1 === sortedStr2;
+  }
 }
 
-module.exports = isAnagram;
+// Create an interface for reading input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Read user input and invoke the isAnagram function
+rl.question("Enter string 1: ", function (str1) {
+  rl.question("Enter string 2: ", function (str2) {
+    const result = isAnagram(str1, str2);
+    console.log(result);
+    rl.close();
+  });
+});
